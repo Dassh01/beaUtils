9015a805bb5712dfcfd911c7a979fc7000792da5
Dassh01 <gilfixbea@gmail.com>
2024-11-18 23:43:50 -0800

    Documentation & Tests

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Main extends beaUtils {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         //Test of beaUtils askForThing
-        System.out.println(askForThing(("Echo: "),Scanner::nextLine,scanner)); //PASS as of 11/16/24
+        //PASS as of 11/16/24
+        //System.out.println(askForThing(("Echo: "),Scanner::nextLine,scanner));
+
+        //Test of version A of method userInputBoolean
+        //PASS as of 11/18/24
+        //System.out.println(userInputBoolean(askForThing("(Yes/No): ",Scanner::nextLine,scanner))); //If input yes of any kind should return true, if no then should return false
+
+        //Test of version B of method userInputBoolean
+        //PASS as of 11/18/24
+        HashSet<String> trueValues = new HashSet<>();
+        trueValues.add("y");
+        trueValues.add("yes");
+        trueValues.add("true");
+        trueValues.add("t");
+        System.out.println(userInputBoolean(askForThing("(Yes/No): ",Scanner::nextLine,scanner),trueValues));
     }
 }
--- beaUtils.java
+++ beaUtils.java
@@ -6,9 +6,9 @@ public class beaUtils {
     /**
      * Comes from retyping user prompts and scanner methods over and over in APCSA projects :C
      * Used to simplify above-mentioned process, does the prompt and scan all in one
-     * @param askText Prompt the user is asked in console
-     * @param inputFunction Scanner lambda method (i.e: Scanner::nextLine, Scanner::nextInt), dependent on what data type you want the function to return as its return type is generic
-     * @param scanner Scanner (hopefully connected to System input...)
+     * @param askText [String] Prompt the user is asked in console
+     * @param inputFunction [Scanner] lambda method (i.e: Scanner::nextLine, Scanner::nextInt), dependent on what data type you want the function to return as its return type is generic
+     * @param scanner [Scanner] (hopefully connected to System input...)
      * @return Returns data type dependent on the scanner lambda inserted
      */
     protected static <T> T askForThing(String askText, Function<Scanner, T> inputFunction, Scanner scanner) {
@@ -30,11 +30,23 @@ public class beaUtils {
         return returningInformation;
     }
 
+    /**
+     * Interprets a string into a boolean. Worked with a (yes/no) or a (y/n) prompt, case-insensitive
+     * @param userInput [String] Raw user input as answer to a prompt similar to the ones listed above
+     * @return [Boolean] true IF user inputted something that looks like a yes, false if user inputted something that looks something other than a yes
+     */
     protected static boolean userInputBoolean(String userInput) {
         String formattedUserInput = userInput.toLowerCase().trim();
-        return formattedUserInput.equals("y") || formattedUserInput.equals("yes");
+        return formattedUserInput.equals("y") || formattedUserInput.contains("yes");
     }
 
+    /**
+     * Interprets a string into a boolean. Worked with a (yes/no) or a (y/n) prompt, case-insensitive.
+     * The more manual/specific version that allows you to choose values that will return true
+     * @param UserInput [String] Raw user input as answer to a prompt similar to the ones listed above
+     * @param trueValues [HashSet] Values that, if user has entered the same, will make the method return true
+     * @return [Boolean] true IF user inputted something that looks like a yes by the standards of the HashSet, false if user inputted something that looks something other than a yes
+     */
     protected static boolean userInputBoolean(String UserInput, HashSet<String> trueValues) {
         String formattedUserInput = UserInput.toLowerCase().trim();
         return trueValues.contains(formattedUserInput);
